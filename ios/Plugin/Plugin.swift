83bad25cbeedb5125b1d4829ad0ea4a63a886636
IOS <github@minhkhoaasia.com>
2021-01-26 18:39:41 +0700

    add ios code

--- ios/Plugin/Plugin.swift
+++ ios/Plugin/Plugin.swift
@@ -8,13 +8,6 @@ import MomoiOSSwiftSdk
 @objc(Momo)
 public class Momo: CAPPlugin {
 
-    var call: CAPPluginCall
-
-    override func viewDidLoad() {
-	    // Do any additional setup after loading the view.
-        NotificationCenter.default.removeObserver(self, name: NSNotification.Name(rawValue: "NoficationCenterTokenReceived"), object: nil)
-        NotificationCenter.default.addObserver(self, selector: #selector(self.NoficationCenterTokenReceived), name:NSNotification.Name(rawValue: "NoficationCenterTokenReceived"), object: nil)
-    }	 
 
     @objc override public func load() {
         // Called when the plugin is first constructed in the bridge
@@ -24,20 +17,17 @@ public class Momo: CAPPlugin {
 
 
     @objc func openMomoApp(_ call: CAPPluginCall) {
-        self.call = call;
         let paymentinfo = NSMutableDictionary()
         
-        paymentinfo["merchantcode"] = "CGV01"
-        paymentinfo["merchantname"] = "CGV Cinemas"
+        paymentinfo["merchantcode"] = "MOMOZLRN20190911"
+        paymentinfo["merchantname"] = "HORAFARM"
         paymentinfo["merchantnamelabel"] = "Service"
         paymentinfo["orderId"] = "012345XXX"
         paymentinfo["orderLabel"] = "OrderID"
         paymentinfo["amount"] = 20000
         paymentinfo["fee"] = 0
         paymentinfo["description"] = "Thanh toán vé xem phim"
-        paymentinfo["extra"] = "{\"key1\":\"value1\",\"key2\":\"value2\"}"
-        paymentinfo["username"] = payment_username //user id/user identify/user email
-        paymentinfo["appScheme"] = "partnerSchemeId"   //partnerSchemeId provided by MoMo , get from business.momo.vn
+        paymentinfo["appScheme"] = "momozlrn20190911"
         MoMoPayment.createPaymentInformation(info: paymentinfo)
         MoMoPayment.requestToken()
     }
@@ -45,13 +35,7 @@ public class Momo: CAPPlugin {
     @objc func NoficationCenterTokenReceived(notif: NSNotification) {
             //Token Replied - Call Payment to MoMo Server
             print("::MoMoPay Log::Received Token Replied::\(notif.object!)")
-            //lblMessage.text = "RequestToken response:\n  \(notif.object as Any)"
-            
-            let response:NSMutableDictionary = notif.object! as! NSMutableDictionary
-            self.call.resolve();
-            let _reference_orderId = response["orderId"] as! String
-            let _statusStr = "\(response["status"] as! String)"
-            let _messageStr = "\(response["message"] as! String)"	    
+            print("We have somehitng")
     }
 
 }
